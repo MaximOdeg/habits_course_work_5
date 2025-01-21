33d1950b4a69560bb5e60bcb98848d3d397d97cb
Max Odegov <zoomzooom228@mail.ru>
2025-01-21 20:46:39 +0700

    ci_23

--- config/settings.py
+++ config/settings.py
@@ -79,8 +79,8 @@ DATABASES = {
         "NAME": "habits",
         "USER": os.getenv("POSTGRES_USER"),
         "PASSWORD": os.getenv("POSTGRES_PASSWORD"),
-        "HOST": os.getenv("POSTGRES_HOST"),
-        "PORT": os.getenv("POSTGRES_PORT"),
+        "HOST": "localhost",
+        "PORT": "5432",
     }
 }
 

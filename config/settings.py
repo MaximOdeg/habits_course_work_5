a3e2e9ba8684eaf6c2fdf9ed7d9f3ce6ed6b9e7c
Max Odegov <zoomzooom228@mail.ru>
2025-01-21 19:49:31 +0700

    ci_12

--- config/settings.py
+++ config/settings.py
@@ -76,7 +76,7 @@ WSGI_APPLICATION = "config.wsgi.application"
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql_psycopg2",
-        "NAME": os.getenv("POSTGRES_DB"),
+        "NAME": "habits",
         "USER": os.getenv("POSTGRES_USER"),
         "PASSWORD": os.getenv("POSTGRES_PASSWORD"),
         "HOST": os.getenv("POSTGRES_HOST"),
